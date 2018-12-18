f8b0e1d8504176ce63c873d8b5e1e4d124d19c75
zeropaul <landerspaulzero@gmail.com>
2018-12-18 13:04:57 -0500

    Loop

--- password.py
+++ password.py
@@ -7,3 +7,14 @@ if (typedPassword == secretPassword):
         print('That password is one that an idiot puts on their luggage.')
 else:
     print('Access denied')
+
+while True:
+    print('Who are you?')
+    name = input('Name: ')
+    if name != 'Zero':
+        continue
+    print('Hi, Zero. What is the password? (It is a Inf..)')
+    password = input('Password: ')
+    if password == 'infinity':
+        break
+print('Access granted Zero')
