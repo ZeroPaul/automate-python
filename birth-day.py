4c752575b58f6816c1e11a04d6142a5a8aaaf851
zeropaul <landerspaulzero>
2019-03-22 17:52:27 -0500

    Five

---
+++ birth-day.py
@@ -0,0 +1,8 @@
+
+birthdays = { 'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4' }
+
+while True:
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == '':
+        break
