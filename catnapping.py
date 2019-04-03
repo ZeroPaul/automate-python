56293eaeaa3ceb31000fb235d223b7501bfce10b
ZeroPaul <landerspaulzero@gmail.com>
2019-04-03 10:29:39 -0500

    S6

---
+++ catnapping.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('''Dear Xo,
+
+Eve's cat has arrested for catnapping, cat burglary, and extortion.
+
+Sincelery,
+Paul''')
+
+print('Dear Xo,\n\nEve\'s cat has been arrested for catnapping, cat burglary, and extortion.\n\nSincelery,\nPaul')
